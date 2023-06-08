8394153754534b09814a5f81679c03049553d0a4
Ovadya33 <timurgts2710@gmail.com>
2023-06-08 13:02:41 +0300

    added button filter

--- src/ui/FilterUI.js
+++ src/ui/FilterUI.js
@@ -16,15 +16,37 @@ export default class FilterUI {
 
     initializeFilterUI() {
         this.filterModal.innerHTML = `
-            <div class="modal-content">
-                <span id="filterCloseBtn" class="close">&times;</span>
-                <h2>Filter Settings</h2>
-                <!-- Add your filter settings and buttons here -->
-                <button id="resetFilterBtn" class="button">Reset Filter</button>
-                <button id="applyFilterBtn" class="button">Apply Filter</button>
-               
+        <div class="modal-content">
+            <span id="filterCloseBtn" class="close">&times;</span>
+            <h2>Filter Settings</h2>
+
+            <div class="filter-section">
+                <label for="genreWithFilter">With Genre:</label>
+                <select id="genreWithFilter">
+                    <option value="">Select genre</option>                    
+                    <!-- Здесь будут варианты выбора категорий фильмов -->
+                </select>
+
+                <label for="genreWithoutFilter">Without Genre:</label>
+                <select id="genreWithoutFilter">
+                    <option value="">Select genre</option>                    
+                    <!-- Здесь будут варианты выбора категорий фильмов -->
+                </select>
+            </div>            
+
+            <div class="filter-section">
+                <label for="yearFromFilter">Year From:</label>
+                <input type="text" id="yearFromFilter" placeholder="Select year">
             </div>
-        `;
+            <div class="filter-section">
+                <label for="yearToFilter">Year To:</label>
+                <input type="text" id="yearToFilter" placeholder="Select year">
+            </div>
+
+            <button id="resetFilterBtn" class="button">Reset Filter</button>
+            <button id="applyFilterBtn" class="button">Apply Filter</button>
+        </div>
+    `;
 
         const closeBtn = document.getElementById('filterCloseBtn');
         closeBtn.addEventListener('click', this.closeModal);  
@@ -39,6 +61,7 @@ export default class FilterUI {
     applyFilterSettings() {
         // Handle the filter settings and apply them
         console.log('Filter settings applied');
+        return true;
     }
 
     resetFilterSettings() {
